f8b409c02704a9643b6c6826b7ea75ab3ac23347
laelaolsen <94853378+laelaolsen@users.noreply.github.com>
2024-01-10 20:11:38 -0700

    Update App.js
    Added input boxes, buttons

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to the Automated Safety Escort System App!</Text>
-      <Text>Input desired pickup and dropoff locations</Text>
+      <Text style={styles.welcomeText}>Welcome to the Automated Safety Escort System App!</Text>
+      <Text style={styles.smallText}>Input desired pickup and dropoff locations.</Text>
+      <TextInput
+      style={styles.input}
+      placeholder="Pickup location"
+      />
+      <Button
+        color="#345995"
+        title="Submit 1"
+        onPress={() => {
+          // Handle button press for text 1
+        }}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Dropoff location"
+      />
+      <Button
+        color="#345995"
+        style={styles.button}
+        title="Submit 2"
+        onPress={() => {
+          // Handle button press for text 2
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +40,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#317e33',
     alignItems: 'center',
     justifyContent: 'space-evenly',
+  },
+  welcomeText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 44,
+  },
+  smallText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  input: {
+    height: 40,
+    width: 400,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 0,
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: 'white',
   },
 });
